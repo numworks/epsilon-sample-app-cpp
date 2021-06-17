0aa54d167e8bf3ec147f18c12b9e48c2fb7d1af1
Émilie Feral <emilie.feral@numworks.com>
2021-06-17 17:36:41 +0200

    [eadk] eadk requires stdint.h

--- eadk/eadk.h
+++ eadk/eadk.h
@@ -1,10 +1,7 @@
 #ifndef EADK_H
 #define EADK_H
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
+#include <stdint.h>
 
 // Backlight
 uint8_t eadk_backlight_brightness();
