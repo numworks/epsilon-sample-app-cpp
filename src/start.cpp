e86e2327ac48f40025c98b8bfce0e747423f39da
Emilie Feral <emilie.feral@numworks.com>
2022-06-21 13:31:50 +0200

    [src] Forbid static C++ object constructors

--- src/start.cpp
+++ src/start.cpp
@@ -28,25 +28,15 @@ void _eadk_start() {
   size_t bssSectionLength = (&_bss_section_end_ram - &_bss_section_start_ram);
   memset(&_bss_section_start_ram, 0, bssSectionLength);
 
-  /* Call static C++ object constructors
-   * The C++ compiler creates an initialization function for each static object.
-   * The linker then stores the address of each of those functions consecutively
-   * between _init_array_start and _init_array_end. So to initialize all C++
-   * static objects we just have to iterate between theses two addresses and
-   * call the pointed function. */
-#define SUPPORT_CPP_GLOBAL_CONSTRUCTORS 1
-#if SUPPORT_CPP_GLOBAL_CONSTRUCTORS
-  for (cxx_constructor * c = (cxx_constructor *)&_init_array_start; c < (cxx_constructor *)&_init_array_end; c++) {
-    (*c)();
-  }
-#else
-  /* In practice, static initialized objects are a terrible idea. Since the init
-   * order is not specified, most often than not this yields the dreaded static
-   * init order fiasco. How about bypassing the issue altogether? */
+  /* Assert that we don't rely on any static C++ object constructor because:
+   * - The NumWorks app uploader doest not expect an init_array section
+   * - In practice, static initialized objects are a terrible idea. Since the
+   *   init order is not specified, most often than not this yields the dreaded
+   *   static init order fiasco. */
   if (&_init_array_start != &_init_array_end) {
     return;
   }
-#endif
+
   // Jump to main
   eadk_main();
 }
